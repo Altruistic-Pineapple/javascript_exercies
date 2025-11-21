b305d1f643ee5485049206e188ed7ac18a7f30dd
Will Richter <converserichter@gmail.com>
2025-11-21 11:22:41 -0800

    11/21/25

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -29,6 +29,33 @@ console.log(mixedJuice);
 const orangeJuice = fruitBlender(0, 8);
 console.log(orangeJuice);
 
+function calculateTotal(Cost, Tax) {
+    return Cost + (Cost * Tax);
+}
+
+console.log(calculateTotal(5, .08));
+
+
+function numberAdded(num1, num2) {
+    return num1 + num2;
+}
+console.log(numberAdded(1, 2));
+
+console.log(numberAdded(5, 6));
+
+
+function numberDivided(number1, number2) {
+    return number1 / number2
+}
+console.log(numberDivided(10, 5))
+
+function numberSubtracted(numb3r1, numb3r2) {
+    return numb3r1 - numb3r2;
+}
+console.log(numberSubtracted(10, 5))
+
+
+
 
 //function declaration
 function calcAge1(birthYear) {
@@ -51,3 +78,28 @@ const calcAge3 = birthYear => 2045 - birthYear;
 
 const age3 = calcAge3(1979)
 console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2021 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firtstName} retires in ${retirement} years`;
+}
+
+const retirement = yearsUntilRetirement(1979, "Will");
+console.log(retirement);
+
+//function within a fumction
+/*
+function cutFruitPieces(fruit) {
+    return fruit = 4;
+}
+
+
+function fruitBlender(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitBlender(5, 2);
+console.log(appleJuice);*/
